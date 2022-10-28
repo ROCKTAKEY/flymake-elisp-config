f9874d5608a1a106f866f1f125455708f24f9755
ROCKTAKEY <rocktakey@gmail.com>
2022-10-29 00:54:10 +0900

    Initial commit

---
+++ flymake-elisp-config.el
@@ -0,0 +1,38 @@
+;;; flymake-elisp-config.el --- Flymake on Emacs Lisp mode in init.el  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  ROCKTAKEY
+
+;; Author: ROCKTAKEY <rocktakey@gmail.com>
+;; Keywords: lisp
+
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "24.1"))
+;; URL: https://github.com/ROCKTAKEY/flymake-elisp-config
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Flymake on Emacs Lisp mode in init.el
+
+;;; Code:
+
+(defgroup flymake-elisp-config ()
+  "Flymake on Emacs Lisp mode in init.el."
+  :group 'lisp
+  :prefix "flymake-elisp-config-"
+  :link '(url-link "https://github.com/ROCKTAKEY/flymake-elisp-config"))
+
+(provide 'flymake-elisp-config)
+;;; flymake-elisp-config.el ends here
---
+++ test/flymake-elisp-config-test.el
@@ -0,0 +1,40 @@
+;;; flymake-elisp-config-test.el --- Test for flymake-elisp-config
+
+;; Copyright (C) 2022  ROCKTAKEY
+
+;; Author: ROCKTAKEY <rocktakey@gmail.com>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Test for flymake-elisp-config
+
+;;; Code:
+
+(require 'ert)
+
+(require 'undercover)
+(undercover "*.el"
+            (:report-format 'codecov)
+            (:report-file "coverage-final.json")
+            (:send-report nil))
+
+(require 'flymake-elisp-config)
+
+
+
+
+(provide 'flymake-elisp-config-test)
+;;; flymake-elisp-config-test.el ends here
